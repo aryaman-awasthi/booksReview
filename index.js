17cb531e42ea9e4420fc8b5454c559e3810ea816
aryaman-awasthi <aryaman.awasthi79@gmail.com>
2023-08-14 22:18:17 +0530

    refractor: index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const session = require('express-session')
+const customer_routes = require('./router/auth_users.js').authenticated;
+const gen_routes = require('./router/general.js').general;
+
+
+let users = []
+//Function to check if the user exists
+const doesExist = (username)=>{
+    let usersWithSameName = users.filter((user)=>{
+        return user.username === username
+    });
+    return usersWithSameName.length > 0;
+}
+//Function to check if the user is authenticated
+const authenticatedUser = (username,password)=>{
+    let validUsers = users.filter((user)=>{
+        return (user.username === username && user.password === password)
+    });
+    return validUsers.length > 0;
+}
+
+
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+
+app.use("/customer/auth/*", function auth(req,res,next){
+//Write the authenication mechanism here
+    if(req.session.authorization) { //get the authorization object stored in the session
+        token = req.session.authorization['accessToken']; //retrieve the token from authorization object
+        jwt.verify(token, "access",(err,user)=>{ //Use JWT to verify token
+            if(!err){
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+        });
+    } else {
+        return res.status(403).json({message: "User not logged in"})
+    }
+});
+
+
+
+
+const PORT =5000;
+
+app.use("/customer", customer_routes);
+app.use("/", gen_routes);
+
+app.listen(PORT,()=>console.log("Server is running", PORT));
