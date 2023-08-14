dcf7447d6d7a93bbf2fde8d9d9793f76cb99cfc7
aryaman-awasthi <aryaman.awasthi79@gmail.com>
2023-08-14 22:02:01 +0530

    feat: added booksDB and index.js

---
+++ router/booksDB.js
@@ -0,0 +1,24 @@
+books = {
+    1: {"author": "Chinua Achebe", "title": "Things Fall Apart", "reviews": {}},
+    2: {"author": "Harper Lee", "title": "To Kill a Mockingbird", "reviews": {}},
+    3: {"author": "George Orwell", "title": "1984", "reviews": {}},
+    4: {"author": "J.K. Rowling", "title": "Harry Potter and the Sorcerer's Stone", "reviews": {}},
+    5: {"author": "F. Scott Fitzgerald", "title": "The Great Gatsby", "reviews": {}},
+    6: {"author": "Jane Austen", "title": "Pride and Prejudice", "reviews": {}},
+    7: {"author": "J.R.R. Tolkien", "title": "The Lord of the Rings", "reviews": {}},
+    8: {"author": "Gabriel García Márquez", "title": "One Hundred Years of Solitude", "reviews": {}},
+    9: {"author": "Leo Tolstoy", "title": "War and Peace", "reviews": {}},
+    10: {"author": "Mark Twain", "title": "The Adventures of Huckleberry Finn", "reviews": {}},
+    11: {"author": "Charles Dickens", "title": "Great Expectations", "reviews": {}},
+    12: {"author": "J.D. Salinger", "title": "The Catcher in the Rye", "reviews": {}},
+    13: {"author": "Aldous Huxley", "title": "Brave New World", "reviews": {}},
+    14: {"author": "Toni Morrison", "title": "Beloved", "reviews": {}},
+    15: {"author": "Mary Shelley", "title": "Frankenstein", "reviews": {}},
+    16: {"author": "Ernest Hemingway", "title": "The Old Man and the Sea", "reviews": {}},
+    17: {"author": "Virginia Woolf", "title": "Mrs. Dalloway", "reviews": {}},
+    18: {"author": "Fyodor Dostoevsky", "title": "Crime and Punishment", "reviews": {}},
+    19: {"author": "Albert Camus", "title": "The Stranger", "reviews": {}},
+    20: {"author": "Hermann Hesse", "title": "Siddhartha", "reviews": {}}
+}
+
+module.exports = books;
